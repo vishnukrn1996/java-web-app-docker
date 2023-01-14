59649ccedf6ad96db65e1506ed8d09ef9863a243
vishnukrn1996 <98821332+vishnukrn1996@users.noreply.github.com>
2023-01-14 19:20:20 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello Docker";
+			return "Hello vishnu";
 		}else{
 			return "Hello " + name;
 		}
